ad5cc71b5b929c61a6eff2bcf1de94b3202e4951
HuydDo <huydinhdo@yahoo.com>
2020-06-05 16:17:36 -0600

    modified time.js

--- time.js
+++ time.js
@@ -40,3 +40,18 @@ var myVar = setInterval(myTimer ,1000);
 function myTimer() {
 var d = new Date();
 document.getElementById("demo1").innerHTML = d.toLocaleTimeString();
+}
+
+function move() {
+  var elem = document.getElementById("myBar");
+  var width = 0;
+  var id = setInterval(frame, 100);
+  function frame() {
+  if (width == 100) {
+  clearInterval(id);
+  } else {
+  width++;
+  elem.style.width = width + '%';
+  }
+ }
+}
