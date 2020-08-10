89c5b87ad934f97a48a67177cba5c331a6e40614
HuydDo <huydinhdo@yahoo.com>
2020-08-10 10:53:06 -0600

    modified arrayDuplicate.js

--- arrayDuplicate.js
+++ arrayDuplicate.js
@@ -1,6 +1,6 @@
 // https://codippa.com/how-to-check-if-array-contains-duplicate-values-in-javascript/
 // Method 1 Using an Object
-checkDuplicate();
+// checkDuplicate();
    function checkDuplicate() {
       let arr = ["abc","xy","bb", "axz", "abc"];
       // empty object
@@ -15,6 +15,24 @@ checkDuplicate();
          }
          // add entry in object with the element as key
          map[arr[i]] = true;
+      }
+      if(result) {
+         console.log('Array contains duplicate elements');
+      } else {
+         console.log('Array does not contain duplicate elements');
+      }
+   }
+
+   //Method 2 Using a Set
+   checkDuplicate2();
+   function checkDuplicate2() {
+      let arr = ["abc","xy","bb", "abc"];
+      let result = false;
+      // create a Set with array elements
+      const s = new Set(arr);
+      // compare the size of array and Set
+      if(arr.length !== s.size){
+         result = true;
       }
       if(result) {
          console.log('Array contains duplicate elements');
