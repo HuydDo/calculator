b33cc57078ed0a496fc15d84af217f5b49066e3f
HuydDo <huydinhdo@yahoo.com>
2020-12-29 00:21:03 -0700

    relocated folders and added C# Fundamental folder

---
+++ C# Fundamentals/gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 34.1;
+            double y = 32.3;
+           
+            Console.WriteLine(x+y);
+
+            if(args.Length > 0)
+            {
+              Console.WriteLine($"Hello, {args[0]} !");
+            }
+            else 
+            {
+              Console.WriteLine("Hello!"); 
+            }
+        }
+    }
+}
+
+//dotnet run -- Huy
---
+++ Revature/C#/programmingStructure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace programmingStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
